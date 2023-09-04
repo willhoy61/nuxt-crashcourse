6c7c6e7f739fe6bfb29e3dc45485e7e545582662
willhoy61 <willhoy61@gmail.com>
2023-09-04 14:59:53 -0500

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <h1>Home Nuxt</h1>
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Repellat vitae eos vero aspernatur deleniti. Sint tenetur laboriosam nam dolores saepe.</p>
+
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Repellat vitae eos vero aspernatur deleniti. Sint tenetur laboriosam nam dolores saepe.</p>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+h1 {
+    margin-bottom: 20px;
+    font-size: 36px;
+}
+p {
+    margin: 20px 0;
+}
+</style>
---
+++ pages/products/index.vue
@@ -0,0 +1,36 @@
+<template>
+    <div>
+       <div class="grid grid-cols-4 gap-5">
+        <div v-for="p in products">
+            <ProductCard :product="p" />
+        </div>
+       </div>
+    </div>
+</template>
+
+<script setup>
+definePageMeta({
+    layout: 'products'
+})
+
+const { data: products } = await useFetch('https://fakestoreapi.com/products')
+
+useHead({
+    title: 'Nuxt Dojo | Merch',
+    meta: [
+        {
+            name: 'description', content: 'Nuxt 3 Merch'
+        }
+    ]
+})
+</script>
+
+<style scoped>
+h1 {
+    margin-bottom: 20px;
+    font-size: 36px;
+}
+p {
+    margin: 20px 0;
+}
+</style>
